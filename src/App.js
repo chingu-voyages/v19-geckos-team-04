657106175da8c3c16da127c078047484c491a21e
Abigail Swarth <abigailswarth@gmail.com>
2020-05-16 12:58:23 -0400

    oauth spotify login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    
+    state = { 
+        serverData: {},
+        filterString: ''
+    }
+    
+    componentDidMount() {
+        
+    }
+    
+    render() {
+      return (
+        <div className="App">
+        { this.state.serverData.user ? 
+            <span>Test</span>
+            :
+            <bitton onClick={ () => window.location = 'http://geckos4-backend.herokuapp.com/login' }>Sign In</bitton>
+        }
+        </div>
+      );
+    }
 }
+
 
 export default App;
