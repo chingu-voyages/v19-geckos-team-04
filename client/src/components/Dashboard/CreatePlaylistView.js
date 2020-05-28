56e7461cd6719d1d2d962b02b05e113935a2d5e9
jbonilla8 <jalisabonilla10@gmail.com>
2020-05-28 19:01:33 -0400

    more work on filter controls, created expand and collapse buttons for filter area

--- client/src/components/Dashboard/CreatePlaylistView.js
+++ client/src/components/Dashboard/CreatePlaylistView.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { DarkTheme } from '../Shared/Styles/DarkTheme';
+import CollapseUpArrow from '../../images/collapse-up-arrow.svg';
+import ExpandDownArrow from '../../images/expand-down-arrow.svg';
 
 const CreatePlaylistView = () => {
+  const [collapse, setCollapse] = useState(false);
+
   return (
     <CreatePlaylistViewContainer>
-      <FilterControls>
+      <FilterControls collapse={collapse}>
         <Category>
-          <Label>Category</Label>
+          <FilterLabel>Category</FilterLabel>
           <RadioButtons>
             <input type="radio" id="warmup" value="warmup" name="category" />
             <label for="warmup">Warm-up</label>
@@ -22,7 +26,28 @@ const CreatePlaylistView = () => {
             <label for="cooldown">Cool-down</label>
           </RadioButtons>
         </Category>
+        <BPM>
+          <FilterLabel>BPM</FilterLabel>
+        </BPM>
+        <Genre>
+          <FilterLabel>Genre</FilterLabel>
+        </Genre>
+        <Time>
+          <FilterLabel>Time</FilterLabel>
+        </Time>
       </FilterControls>
+      <HorizontalRule />
+      <CollapseExpand onClick={() => setCollapse(!collapse)}>
+        {collapse ? (
+          <span>
+            Expand <img src={ExpandDownArrow} />
+          </span>
+        ) : (
+          <span>
+            Collapse <img src={CollapseUpArrow} />
+          </span>
+        )}
+      </CollapseExpand>
     </CreatePlaylistViewContainer>
   );
 };
@@ -37,14 +62,14 @@ const CreatePlaylistViewContainer = styled.div`
 `;
 
 const FilterControls = styled.div`
-  display: grid;
+  display: ${props => (props.collapse ? 'none' : 'grid')};
   padding-top: 2rem;
   grid-template-rows: 50% 50%;
   grid-template-columns: 50% 50%;
   height: 15vw;
 `;
 
-const Label = styled.label`
+const FilterLabel = styled.label`
   color: ${DarkTheme.lightgray};
 `;
 
@@ -55,4 +80,64 @@ const Category = styled.div`
 
 const RadioButtons = styled.div`
   color: ${DarkTheme.orange};
+  margin-top: 1rem;
+
+  input[type='radio'] {
+    visibility: hidden;
+    position: absolute;
+  }
+
+  input[type='radio'] + label:before {
+    height: 1rem;
+    width: 1rem;
+    margin-right: 2px;
+    content: ' ';
+    display: inline-block;
+    vertical-align: baseline;
+    border: 2px solid ${DarkTheme.orange};
+    margin-right: 0.5rem;
+  }
+
+  input[type='radio']:checked + label:before {
+    background: ${DarkTheme.orange};
+  }
+
+  input[type='radio'] + label:before {
+    border-radius: 50%;
+  }
+
+  label {
+    margin-right: 1rem;
+  }
+`;
+
+const BPM = styled.div`
+  grid-row: 2/3;
+  grid-column: 1/2;
+`;
+
+const Genre = styled.div`
+  grid-row: 1/2;
+  grid-column: 2/3;
+`;
+
+const Time = styled.div`
+  grid-row: 2/3;
+  grid-column: 2/3;
+`;
+
+const HorizontalRule = styled.div`
+  color: ${DarkTheme.lightgray};
+  border: 1px solid ${DarkTheme.charcoal};
+`;
+
+const CollapseExpand = styled.div`
+  text-align: right;
+  font-size: 0.9rem;
+  cursor: pointer;
+
+  img {
+    width: 1.2rem;
+    height: 1.2rem;
+  }
 `;
