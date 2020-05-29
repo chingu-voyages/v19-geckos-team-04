8addd77b5b7bbd6e0410b6015685a60e9cb5f421
ghost <ghostintheboot@protonmail.com>
2020-05-29 18:09:38 -0500

    pre-merge data added in App.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import queryString from 'query-string';
 import './App.scss';
 import Header from './../LoggedOut/Header';
@@ -8,9 +9,28 @@ import TourButton from '../LoggedOut/TourButton';
 import SignInButton from '../LoggedOut/SignInButton';
 import SoundBars from '../LoggedOut/SoundBars';
 import PseudoNavbar from '../LoggedOut/PseudoNavbar';
-// import Modal from '../Shared/UI/Modal';
 import Dashboard from '../Dashboard/Dashboard';
 import { ModalProvider } from 'styled-react-modal';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
+
+const LandingContainer = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+`;
+
+const HeaderContainer = styled.div`
+  position: relative;
+  top: 20%;
+  z-index: 1000;
+`;
+
+const ButtonsContainer = styled.div`
+  text-align: center;
+`;
 
 // Styles for dark and light modes, respectively.
 const GlobalStyle = createGlobalStyle`
@@ -48,14 +68,15 @@ class App extends Component {
   }
 
   componentDidMount() {
-      let parsed = queryString.parse( window.location.search );
-      let accessToken = parsed.access_token;
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
 
-      //❗ TypeError: Cannot read property '0' of undefined ❗
-      fetch( 'https://api.spotify.com/v1/me', {
-          headers: { 'Authorization' : 'Bearer ' + accessToken}
-      } ).then( ( res ) => res.json() )
-      .then( data => this.setState( { serverData: { user: data } } ) )
+    //❗ TypeError: Cannot read property '0' of undefined ❗
+    fetch('https://api.spotify.com/v1/me', {
+      headers: { Authorization: 'Bearer ' + accessToken }
+    })
+      .then(res => res.json())
+      .then(data => this.setState({ serverData: { user: data } }));
   }
 
   onOpenModal() {
@@ -86,14 +107,14 @@ class App extends Component {
                 changeTheme={this.handleClick}
                 />
               <Header />
-            <ModalProvider >
-              <div style={{textAlign: 'center'}}>
-                <TourButton
-                  showModal={() => this.onOpenModal()}
-                />
-                <SignInButton/>
-              </div>
-            </ModalProvider>
+              <ModalProvider >
+                <div style={{textAlign: 'center'}}>
+                  <TourButton
+                    showModal={() => this.onOpenModal()}
+                  />
+                  <SignInButton/>
+                </div>
+              </ModalProvider>
               <SoundBars/>
             </>
           }
