a0ef521bc0781b19b433020bcfa2c17a1181495b
jbonilla8 <jalisabonilla10@gmail.com>
2020-06-17 20:51:32 -0400

    trigger deploy

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/auth', authRoutes);
 
 app.use('/api', apiRoutes);
-
+ 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
