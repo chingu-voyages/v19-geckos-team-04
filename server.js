4d017594f8b9b34966ab018b71dacc781b1aebec
jbonilla8 <jalisabonilla10@gmail.com>
2020-06-17 20:31:42 -0400

    change for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const authRoutes = require("./routes/auth-routes");
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 const apiRoutes = require('./routes/api-routes');
-const passportSetup = require("./config/passport-setup");
+const passportSetup = require('./config/passport-setup');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
 const mongoose = require('mongoose');
@@ -16,10 +16,12 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // set up cookies session
-app.use(cookieSession({
+app.use(
+  cookieSession({
     maxAge: process.env.COOKIE_AGE,
-    keys: [process.env.COOKIE_KEY],
-}))
+    keys: [process.env.COOKIE_KEY]
+  })
+);
 
 // initialize passport
 app.use(passport.initialize());
@@ -28,16 +30,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // connect to mongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('Connected to mongoDB.')
-})
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to mongoDB.');
+  }
+);
 
 //set up routes
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes);
 
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes);
 
-const port = (process.env.PORT || 8888);
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 8888;
 
 app.listen(port, () =>
   console.log(
